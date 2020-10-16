fd6f4f816442da6f8642663cdbbd55c036c992df
Ханк <hankdjambong@gmail.com>
2020-10-16 03:40:39 +0300

    Commit for Lab_4_5.

---
+++ src/main/java/Lab4/PrimaryColors.java
@@ -0,0 +1,17 @@
+package Lab4;
+
+public enum PrimaryColors {
+    BLACK, RED, BLUE, GREEN, CYAN, MAGENTA, YELLOW, WHITE;
+
+    public static PrimaryColors getRed() {
+        return RED;
+    }
+
+    public static PrimaryColors getGreen() {
+        return GREEN;
+    }
+
+    public static PrimaryColors getBlue() {
+        return BLUE;
+    }
+}
